30485b75089567b60a10872d8fe0bdafcdecedfc
kimrix381 <kimrix381@gmail.com>
2025-04-14 11:49:34 +0300

    commit 2

---
+++ user-list-app/src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from "react";
+import UserList from "./UserList.jsx";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
---
+++ user-list-app/src/UserList.jsx
@@ -0,0 +1,29 @@
+// src/UserList.jsx
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const UserList = () => {
+  const [listOfUser, setListOfUser] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => setListOfUser(response.data))
+      .catch((error) => console.error("Error fetching users:", error));
+  }, []);
+
+  return (
+    <div>
+      <h2>User List</h2>
+      <ul>
+        {listOfUser.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
